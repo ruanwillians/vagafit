176de253b26fc2c646307949d139785570a639e4
Ruan Willians Mira Nogueira <108192932+ruanwillians@users.noreply.github.com>
2025-08-25 21:47:40 -0300

    first commit

---
+++ src/main/java/com/vagafit/backend/user/UserController.java
@@ -0,0 +1,14 @@
+package com.vagafit.backend.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/users")
+    public String getUsers() {
+        return "Hello from Users!";
+    }
+
+}
